e505a153c719cb739d82ebdf8bcce21db207b758
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-10-26 15:36:07 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import  express  from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express ();
+app.use(express.json());
+
+const MONGODB_URI = "";
+const connectDB = async()=>{
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    if (conn) {
+        console.log ('MONGODB connected');
+    }
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen (PORT, () =>{
+    console.log (`Server running on port : ${PORT}`);
+    connectDB();
+})
