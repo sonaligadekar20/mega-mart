ec29f14822189f58eb97adf56cd8d99b3cd28266
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-10-26 18:57:42 +0530

    signup from added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import  express  from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import User from './models/User.js';
 dotenv.config();
+
 
 const app = express ();
 app.use(express.json());
@@ -14,8 +16,40 @@ const connectDB = async()=>{
     }
 }
 
+// POST signup
+
+app.post("/signup", async (req, res)=>{
+    const {name, email, password, mobile, address, gender} = req.body;
+
+    const user = new User({
+        name: name,
+        email: email,
+        password: password,
+        mobile: mobile,
+        address: address,
+        gender: gender
+    });
+
+    try{
+        const savedUser = await user.save();
+
+        res.json({
+            success: true,
+            data:  savedUser,
+            message: "Signup Successful"
+        })
+    }
+    catch(e){
+        res.json({
+            success: false,
+            message: e.message
+        })
+    }
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen (PORT, () =>{
     console.log (`Server running on port : ${PORT}`);
     connectDB();
-})
+});
---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+import {Schema, model} from 'mongoose';
+
+const userSchema = new Schema({
+   name :{
+    type : String,
+    default :"-"
+   },
+   email : {
+    type : String,
+    unique : true,
+    required : true
+   },
+   password : {
+    type : String,
+    required : true
+   },
+   mobile : {
+    type : String, 
+    unique: true,
+    required : true
+   },
+   address : {
+    type : String
+   },
+   gender : {
+    type : String ,
+    default: "Prefer not to say"
+
+   }
+});
+
+const User = model("User", userSchema);
+
+export default User;
