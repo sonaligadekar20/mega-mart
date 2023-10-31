a6320472bdcc3175d4a74bb17635579e9bf306ed
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-10-31 13:29:36 +0530

    API featch in login form

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,8 +1,61 @@
-import React from 'react'
+import React,{useState} from 'react';
+import axios  from 'axios';
+import "./Login.css";
 
 function Login () {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const login = async()=>{
+        if(!email){
+            alert("Email is required");
+            return;
+        }
+        const response = await axios.post("/login",{
+            email : email,
+            password: password
+        })
+
+        alert(response?.data?.message)
+
+        if(response?.data?.success){
+            alert(response?.data?.success)
+            window.location.href = "/";
+        }
+    };
+    
     return(
-        <div>Login</div>
+        <div>
+            <form className='login-form'>
+                <h1 className='text-center'>Login Form</h1>
+
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="text"
+                    placeholder="Enter your email"
+                    id="email"
+                    className="form-control"
+                    value={email}
+                    onChange={(e) =>{
+                       setEmail(e.target.value);
+                    }}/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password"
+                    placeholder="Enter password"
+                    id="password"
+                    className="form-control"
+                    value={password}
+                    onChange={(e) =>{
+                       setPassword(e.target.value);
+                    }}/>
+                </div>
+
+                <button type="button" className='btn login-btn' onClick={login}>Login</button>
+               
+            </form>
+        </div>
     )
 }
 
