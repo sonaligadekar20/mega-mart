5a48fc064922cb93153a936a7b66472e0c479960
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-11-01 16:01:55 +0530

    media queries added in navbar

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import "./Home.css"
 import Navbar from '../../components/Navbar/Navbar';
+import ProductCard from '../../components/ProductCard/ProductCard';
 
 function Home () {
+    const [products, setProducts] = useState([])
+
+    const loadProducts = async ()=>{
+       try{
+        const response = await axios.get("/products");
+        setProducts(response?.data?.data);
+       }
+       catch(err){
+        console.log(err);
+        alert("Error loading products");
+       }  
+    };
+
+    useEffect(()=>{
+        loadProducts();
+    },[])
+
     return(
         <div>
             <Navbar/>
-            <h1>This is Home Page</h1>
+          
         </div>
     )
 }
