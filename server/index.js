1dcee5b0951adb3a3e4797bfd54d0cc1dc47dcfd
sonaligadekar20 <sonaliygadekar@gmail.com>
2024-08-18 23:35:14 +0530

    project hoisting steps are added

--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+dotenv.config();
+import path from 'path';
+
 import { postApiLogin, postApiSignup } from "./controllers/user.js";
 import { deleteApiProduct, getApiProduct, getApiProductById, getApiProductsBySearch, postApiProduct, putApiProduct } from "./controllers/product.js";
 import { getApiOrderById, getApiOrders, getApiOrderUserById, patchApiOrderStatus, postApiOrder } from "./controllers/order.js";
-dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-const MONGODB_URI = "";
+
+const __dirname = path.resolve();
+
 const connectDB = async () => {
+  try{
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     if (conn) {
         console.log('MONGODB connected');
     }
-}
+  }
+  catch(e){
+    console.log(e.message)
+  }
+};
+connectDB
 
 // POST signup
 app.post('/api/signup', postApiSignup);
@@ -43,19 +53,27 @@ app.delete('/api/product/:id', deleteApiProduct);
 app.put('/api/product/:id', putApiProduct);
 
 // Post/order
-app.post('/order', postApiOrder);
+app.post('/api/order', postApiOrder);
 
 // GET/orders
-app.get('/orders', getApiOrders);
+app.get('/api/orders', getApiOrders);
 
 // GET/order/:id
-app.get('/order/:id', getApiOrderById);
+app.get('/api/order/:id', getApiOrderById);
 
 // GET/orders/user/:id
-app.get('/orders/user/:id', getApiOrderUserById);
+app.get('/api/orders/user/:id', getApiOrderUserById);
   
 // PATCH/order/status/:id
-app.patch('/order/status/:id', patchApiOrderStatus);
+app.patch('/api/order/status/:id', patchApiOrderStatus);
+  
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+    });
+  }
   
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
